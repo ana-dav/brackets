c2b2eba07574cec2d484d5f523eab14edfabf546
ana-dav <opa.super.op@gmail.com>
2020-10-19 01:51:55 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let shallowCopyStr = str
+    for(let i = 0; i < str.length; i++) {
+        for(let j = 0; j < bracketsConfig.length; j++) {
+            let reduceBrackets = bracketsConfig[j][0] + bracketsConfig[j][1]
+            shallowCopyStr = shallowCopyStr.replace(reduceBrackets,'')
+        }
+    }
+    return shallowCopyStr.length == 0
 }
